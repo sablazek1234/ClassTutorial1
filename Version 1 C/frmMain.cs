0d28f90ac1aeb24449b32617b4b1c046e52c0e4e
sablazek1234 <30310110+sablazek1234@users.noreply.github.com>
2018-03-07 12:52:23 +1300

    These are the changes I made

--- Version 1 C/frmMain.cs
+++ Version 1 C/frmMain.cs
@@ -26,7 +26,7 @@ namespace Version_1_C
             string[] lcDisplayList = new string[theArtistList.Count];
 
             lstArtists.DataSource = null;
-            // this is my code
+            // This is my code
             theArtistList.Keys.CopyTo(lcDisplayList, 0);
             lstArtists.DataSource = lcDisplayList;
             lblValue.Text = Convert.ToString(theArtistList.GetTotalValue());
